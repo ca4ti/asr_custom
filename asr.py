4fa17f673ca6747e4f02ae3b9d732e16d8e4d167
ca4ti <36139418+ca4ti@users.noreply.github.com>
2024-02-09 10:37:30 +0100

    Update asr.py

--- asr.py
+++ asr.py
@@ -6,13 +6,17 @@ from vosk import Model, KaldiRecognizer
 import pyaudio
 
 model = Model(r'file')
+#https://alphacephei.com/vosk/models/vosk-model-it-0.22.zip
+#https://alphacephei.com/vosk/models/vosk-model-small-it-0.22.zip
+#https://alphacephei.com/vosk/models/vosk-model-small-pt-0.3.zip
+#https://alphacephei.com/vosk/models/vosk-model-pt-fb-v0.1.1-20220516_2113.zip
 recognizer = KaldiRecognizer(model, 1600)
 
 #Recognize from the microfone
 
 cap = pyaudio.PyAudio()
 stream = cap.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
-stream.start_steam()
+stream.start_stream()
 
 while True:
   data = stream.read(4096)
